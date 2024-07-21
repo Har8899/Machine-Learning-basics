ef195c272b96f3d05e0fbac8ed6d67ecff228ff9
Harshikha <85279763+Har8899@users.noreply.github.com>
2024-07-21 15:38:41 +0530

    Update 04Problem.py

--- 01Loading Data/04Problem.py
+++ 01Loading Data/04Problem.py
@@ -2,7 +2,7 @@
 # load the library
 import pandas as pd
 
-df = pd.read_jsom('data.jsom', orient='colomns')
+df = pd.read_json('data.jsom', orient='colomns')
 
 # view the first two rows
 print(df.head(2))
